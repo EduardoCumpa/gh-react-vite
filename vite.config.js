597a387ad88ba1e8eeab29b358024612c621098e
Eduardo Cumpa <eduardo.cumpa@intelecta.biz>
2022-01-11 12:03:16 -0500

    Fix update vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://EduardoCumpa.github.io/',
+  base: 'https://eduardocumpa.github.io/',
   plugins: [react()]
 })
